4857ba17cefd8181c029a9939c3eb0438af7d458
EugeneMondkar <yatinm@gmail.com>
2021-11-22 06:17:40 -0800

    Updated graph object, crawlers

--- page_graph.py
+++ page_graph.py
@@ -6,5 +6,5 @@ class PageGraph(UserDict):
     def add_page(self, url, num_in_links, in_links, num_out_links, out_links):
         self.data[url] = {"num_in_links": num_in_links,"in_links": in_links, "num_out_links": num_out_links, "out_links": out_links}
 
-# Based of Miro's page rank implementation
+
 

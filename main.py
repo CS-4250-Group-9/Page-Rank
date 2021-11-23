00074ab1b5b1fe20d77cc293cc7b88053b06abf8
EugeneMondkar <yatinm@gmail.com>
2021-11-22 18:52:38 -0800

    Page Rank Computation Completed

--- main.py
+++ main.py
@@ -1,28 +1,30 @@
 from crawlers import scrape_CPP, scrape_NFL
 import page_graph as pg
 
-# graph_cpp = scrape_CPP(100)
+# graph_CPP = scrape_CPP(2000)
 
-# graph_cpp.page_rank(50)
+# graph_CPP.page_rank(50)
 
-# # itr = iter(graph_cpp)
+# # itr = iter(graph_CPP)
 # # first_item = next(itr)
 
-# # for page in graph_cpp:
-# #     print(graph_cpp[page]['page_rank'])
+# # for page in graph_CPP:
+# #     print(graph_CPP[page]['page_rank'])
 
-# sorted_ranks = graph_cpp.get_sorted_rankings()
+# sorted_ranks = graph_CPP.get_sorted_rankings()
 
 
-# with open('page_rankings_cpp.txt', 'w') as file:
+# with open('page_rankings_CPP.txt', 'w') as file:
 
-#     for url in sorted_ranks:
-#         rank = graph_cpp[url]['page_rank']
+#     for i, url in enumerate(sorted_ranks):
+#         rank = graph_CPP[url]['page_rank']
 #         file.write(f"{rank}: {url}\n")
+#         if i >= 99:
+#             break
 
 #########################################################
 
-graph_NFL = scrape_NFL(100)
+graph_NFL = scrape_NFL(2000)
 
 graph_NFL.page_rank(50)
 
@@ -37,7 +39,10 @@ sorted_ranks = graph_NFL.get_sorted_rankings()
 
 with open('page_rankings_NFL.txt', 'w') as file:
 
-    for url in sorted_ranks:
+    for i, url in enumerate(sorted_ranks):
         rank = graph_NFL[url]['page_rank']
         file.write(f"{rank}: {url}\n")
+        if i >= 99:
+            break
+
 

ccc7232ea010ab13e6352bf4565eec74a522a48f
EugeneMondkar <yatinm@gmail.com>
2021-11-22 19:14:32 -0800

    Page Rank Computation Completed

--- main.py
+++ main.py
@@ -1,48 +1,48 @@
 from crawlers import scrape_CPP, scrape_NFL
 import page_graph as pg
 
-# graph_CPP = scrape_CPP(2000)
+graph_CPP = scrape_CPP(20)
 
-# graph_CPP.page_rank(50)
+graph_CPP.page_rank(50)
 
-# # itr = iter(graph_CPP)
-# # first_item = next(itr)
-
-# # for page in graph_CPP:
-# #     print(graph_CPP[page]['page_rank'])
-
-# sorted_ranks = graph_CPP.get_sorted_rankings()
-
-
-# with open('page_rankings_CPP.txt', 'w') as file:
-
-#     for i, url in enumerate(sorted_ranks):
-#         rank = graph_CPP[url]['page_rank']
-#         file.write(f"{rank}: {url}\n")
-#         if i >= 99:
-#             break
-
-#########################################################
-
-graph_NFL = scrape_NFL(2000)
-
-graph_NFL.page_rank(50)
-
-# itr = iter(graph_NFL)
+# itr = iter(graph_CPP)
 # first_item = next(itr)
 
-# for page in graph_NFL:
-#     print(graph_NFL[page]['page_rank'])
+# for page in graph_CPP:
+#     print(graph_CPP[page]['page_rank'])
 
-sorted_ranks = graph_NFL.get_sorted_rankings()
+sorted_ranks = graph_CPP.get_sorted_rankings()
 
 
-with open('page_rankings_NFL.txt', 'w') as file:
+with open('page_rankings_CPP.txt', 'w') as file:
 
     for i, url in enumerate(sorted_ranks):
-        rank = graph_NFL[url]['page_rank']
+        rank = graph_CPP[url]['page_rank']
         file.write(f"{rank}: {url}\n")
         if i >= 99:
             break
+
+#########################################################
+
+# graph_NFL = scrape_NFL(2000)
+
+# graph_NFL.page_rank(50)
+
+# # itr = iter(graph_NFL)
+# # first_item = next(itr)
+
+# # for page in graph_NFL:
+# #     print(graph_NFL[page]['page_rank'])
+
+# sorted_ranks = graph_NFL.get_sorted_rankings()
+
+
+# with open('page_rankings_NFL.txt', 'w') as file:
+
+#     for i, url in enumerate(sorted_ranks):
+#         rank = graph_NFL[url]['page_rank']
+#         file.write(f"{rank}: {url}\n")
+#         if i >= 99:
+#             break
 
 

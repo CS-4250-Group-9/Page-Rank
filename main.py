dec887496f27a3c33112d25a2ce9154a2a26d657
EugeneMondkar <yatinm@gmail.com>
2021-11-24 23:28:33 -0800

    updated main.py

--- main.py
+++ main.py
@@ -2,63 +2,63 @@ from crawlers import scrape_CPP, scrape_NFL
 import page_graph as pg
 
 
-#########################################################
-#
-# Instructions:
-# 
-# Uncomment one of the scrapers (CPP or NFL) below and
-# and run. 
-# 
-# Note: 
-# Both Scrapers cannot be run at the same time.
-#
-#########################################################
-
 ######  CPP Scraper #####################################
 
-# graph_CPP = scrape_CPP(1000)
+def run_CPP():
+    graph_CPP = scrape_CPP(1000)
 
-# graph_CPP.page_rank(50)
+    graph_CPP.page_rank(50)
 
-# sorted_ranks = graph_CPP.get_sorted_rankings()
+    sorted_ranks = graph_CPP.get_sorted_rankings()
 
-# # Validate the Page Rank Total is equal to 1
+    # Validate the Page Rank Total is equal to 1
 
-# with open('page_rankings_CPP.txt', 'w') as file:
+    with open('page_rankings_CPP.txt', 'w') as file:
+        sum = 0
 
-#     sum = 0
-
-#     for i, url in enumerate(sorted_ranks):
-#         rank = graph_CPP[url]['page_rank']
-#         sum += rank
-#         if i <= 99:
-#             file.write(f"{rank}: {url}\n")
+        for i, url in enumerate(sorted_ranks):
+            rank = graph_CPP[url]['page_rank']
+            sum += rank
+            if i <= 99:
+                file.write(f"{rank}: {url}\n")
         
-#     print(f'The total of the page ranks are: {sum}')      
+        print(f'The total of the page ranks are: {sum}')   
 
 ###### End of CPP Scraper ###############################
 
 ######  NFL Scraper #####################################
 
-graph_NFL = scrape_NFL(1000)
+def run_NFL():
+    graph_NFL = scrape_NFL(1000)
 
-graph_NFL.page_rank(50)
+    graph_NFL.page_rank(50)
 
-sorted_ranks = graph_NFL.get_sorted_rankings()
+    sorted_ranks = graph_NFL.get_sorted_rankings()
 
 # Validate the Page Rank Total is equal to 1
 
-with open('page_rankings_NFL.txt', 'w') as file:
+    with open('page_rankings_NFL.txt', 'w') as file:
+        sum = 0
 
-    sum = 0
-
-    for i, url in enumerate(sorted_ranks):
-        rank = graph_NFL[url]['page_rank']
-        sum += rank
-        if i <= 99:
-            file.write(f"{rank}: {url}\n")
+        for i, url in enumerate(sorted_ranks):
+            rank = graph_NFL[url]['page_rank']
+            sum += rank
+            if i <= 99:
+                file.write(f"{rank}: {url}\n")
         
-    print(f'The total of the page ranks are: {sum}')   
-
+        print(f'The total of the page ranks are: {sum}')
+  
 ###### End of NFL Scraper ###############################
 
+if __name__ == '__main__':
+    print("Which crawl would you like to perform:")
+    print("1. Crawl CPP")
+    print("2. Crawl NFL")
+    try:
+        choice = int(input("Enter choice (1 or 2) here: "))
+        if choice == 1:
+            run_CPP()
+        else:
+            run_NFL() 
+    except:
+        print('You need to enter either "1" or "2"')
